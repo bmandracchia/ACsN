47437f42baa8a3ebf7e8206986aba8ac7e9f7ea9
bmandracchia <biagio.mandracchia@gmail.com>
2019-07-17 17:13:41 -0400

    Update ACSN_processing_parallel2.m

--- ACsN_code/Functions/ACSN_processing_parallel2.m
+++ ACsN_code/Functions/ACSN_processing_parallel2.m
@@ -6,7 +6,7 @@ disp('Processing...');
 for frame = 1:size(I,3)
     
     [img(:,:,frame), sigma(frame,:),I1(:,:,frame)] = ACSN_core2(I(:,:,frame),NA,Lambda,PixelSize,Gain,Offset,Window,Hotspot,Level,Mode,SaveFileName);
-        
+    
 end
 
 
@@ -29,34 +29,28 @@ if Video(1) ~= 'n'
         
         sType = 2;
         
-        if sum(size(img)>[32 32 20])  % [256 256 20]
+        size_y = min(Window,size(img,1));
+        size_x = min(Window,size(img,2));
+        size_z = min(100,size(img,3));
+        overlap = 5;
+        
+        Tiles = im2tiles(img,overlap,size_x,size_y,size_z);
+        Sigma = im2tiles(sigma,0,size_z,1);
+        parfor idx = 1:numel(Tiles)
             
-            size_y = min(Window,size(img,1));
-            size_x = min(Window,size(img,2));
-            size_z = min(100,size(img,3));
-            overlap = 5;
+            psd = mean(Sigma{idx}).*ones(8);
             
-            Tiles = im2tiles(img,overlap,size_x,size_y,size_z);
-            parfor idx = 1:numel(Tiles)
-                
-                psd = mean(sigma(:,idx)).*ones(8);
-                
-                Tiles{idx} = Video_filtering_t(Tiles{idx},psd,psd,'dct',0,1,sType,'np',0);
-                
-            end
-            img = tiles2im(Tiles,overlap);
-            clear Tiles;
-            
-        else
-            
-            img = Video_filtering_t(img,psd,psd,'dct',0,1,sType,'np',0);
+            Tiles{idx} = Video_filtering_t(Tiles{idx},psd,psd,'dct',0,1,sType,'np',0);
             
         end
+        img = tiles2im(Tiles,overlap);
+        clear Tiles Sigma;
+        
         
         parfor i = 1:size(img,3)
             M1 = max(max(img(:,:,i)));
             M2 = min(min(img(:,:,i)));
-            beta = mean(sigma).*alpha;
+            beta = mean(sigma(i,:)).*alpha;
             img(:,:,i) = (img(:,:,i) - M2)./(M1 -M2);
             img(:,:,i) = Video_filtering_xy(img(:,:,i),beta);
             img(:,:,i) = (img(:,:,i)).*(M1-M2)+ M2;
